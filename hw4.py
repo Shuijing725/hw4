b831ff30ca14853ffd636b03ce2e5bb5fdfe4332
Shui Jing Liu <minnie725@outlook.com>
2017-07-18 22:34:43 -0500

    hw4 done

--- hw4.py
+++ hw4.py
@@ -6,10 +6,12 @@ class BestAttr(object):
 		self.total = 0 # total number of data
 		self.attr = [] # list of all attributes (not include target attribute)
 		self.data = [] # data (in vertical format)
-		self.info_gain = {} # dictionary of info_gain if we select any attribute
-		self.gain_ratio = {} # dictionary of info_gain if we select any attribute
 
+		self.info_gain = dict() # dictionary of info_gain if we select any attribute
+		self.gain_ratio = dict() # dictionary of info_gain if we select any attribute
+		# self.target_vals = dict() # dictionary of all possible target attribute values with number of data in it
 		self.info_before = 0 # information of all data before classification
+
 
 	# read input from stdin 
 	def read_input(self):
@@ -36,12 +38,15 @@ class BestAttr(object):
 
 	# use all attributes to classify the data and calculate info_gain and info_ratio
 	def calculate(self):
+		# calculate info_before
+		self.find_info_before()
 		# i = index of attribute we are investigating now
 		for i in range(len(self.attr)):
 			# print("attribute:", self.attr[i])
 			# number of distinct values of the current attribute
-			nb_cluster = len(set(self.data[i])) 
-			# dictionary of all distinct values
+			# nb_cluster = len(set(self.data[i])) 
+
+			# dictionary of all distinct values (key: values of current attribute, val: indices of data with attr value = key)
 			cluster = dict()
 
 			# j = index of data
@@ -52,13 +57,22 @@ class BestAttr(object):
 				else:
 					cluster[self.data[i][j]] = [j]
 
-
+			# calculate info_gain for the current cluster
+			self.find_info_gain_ratio(cluster, self.attr[i])
 			# print(cluster)
 
+		# compare info_gain and info_gain_ratio for all classifications and select the best one
+		info_gain_max = max(self.info_gain, key = self.info_gain.get)
+		gain_ratio_max = max(self.gain_ratio, key = self.gain_ratio.get)
+		print(info_gain_max)
+		print(gain_ratio_max)
+			
+
+	# calculate self.info_before
 	def find_info_before(self):
 		target_idx = len(self.attr)
-		# build list of target values
 		target_vals = dict()
+
 		# put the number of each value in dictionary
 		for i in range(self.total):
 			if self.data[target_idx][i] in target_vals:
@@ -70,12 +84,46 @@ class BestAttr(object):
 		# calculate info
 		for key in target_vals:
 			self.info_before -= (target_vals[key] / self.total) * math.log2(target_vals[key] / self.total)
+		# print('info_before:', self.info_before)
 
 
+	# calculate infomation gain for each cluster to fill in self.info_gain
+	def find_info_gain_ratio(self, cluster, attribute):
+		# print("")
+		# print("find info gain ratio with attribute = ", attribute)
+		# print("cluster = ", cluster)
 
+		self.info_gain[attribute] = 0
 
+		# ret_val = dict() 
+		target_idx = len(self.attr)
+		split_info = 0
+		info_after = 0 # stores information after classification
 
+		# loop through all keys in cluster to find info after clustering
+		for key, val in cluster.items():
+			# print('examine key =', key, 'val = ', val)
+			ret_val = dict() 
+			info_after_each = 0  # information for each cluster after classification
+			split_info -= (len(val) / self.total) * math.log2(len(val) / self.total)
+			# for all indices of data in current key
+			for i in val:
+				if self.data[target_idx][i] in ret_val:
+					ret_val[self.data[target_idx][i]] += 1
+				else:
+					ret_val[self.data[target_idx][i]] = 1
 
+			# print('ret_val = ', ret_val)
+			# find info gain of ret_val
+			for key1 in ret_val:
+				info_after_each -= (ret_val[key1] / len(val)) * math.log2(ret_val[key1] / len(val))
+			info_after += (len(val) / self.total) * info_after_each
+			# print("info_after_each:", info_after_each)
+
+		self.info_gain[attribute] = self.info_before - info_after
+		self.gain_ratio[attribute] = self.info_gain[attribute] / split_info
+		# print('info after:', info_after)
+		# print('info_gain:', self.info_gain[attribute])
 
 def main():
 	a = BestAttr()
